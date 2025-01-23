67161dd69584adeec23880bafc325e77d1cf5b54
bedon <dev.bedon@gmail.com>
2025-01-23 23:12:37 +0200

    optimize code

--- src/main/java/org/example/interfaces/LocatorInitializer.java
+++ src/main/java/org/example/interfaces/LocatorInitializer.java
@@ -8,26 +8,24 @@ import java.lang.reflect.Field;
 
 public interface LocatorInitializer {
     default void initializeLocators(Class clazz, Page page) {
-        Field[] fields = clazz.getDeclaredFields();
-        for (Field field : fields) {
-            field.setAccessible(true);
+        for (Field field : clazz.getDeclaredFields()) {
             try {
-                if (field.get(this) == null) {
+                if (field.trySetAccessible() && field.get(this) == null) {
                     if (field.isAnnotationPresent(FindBy.class)) {
                         FindBy annotation = field.getAnnotation(FindBy.class);
-                        String selector = annotation.selector();
-                        field.set(this, page.locator(selector));
+                        field.set(this, page.locator(annotation.selector()));
                     } else if (Locatable.class.isAssignableFrom(field.getType())) {
-                        Class<?> chieldClass = field.getType();
-                        FindBy annotation = chieldClass.getAnnotation(FindBy.class);
-                        String selector = annotation.selector();
-                        BaseLocatedElement element = (BaseLocatedElement) chieldClass.getDeclaredConstructor(Page.class, String.class).newInstance(page, selector);
-                        chieldClass.cast(element);
-                        field.set(this, element);
+                        Class<?> fieldType = field.getType();
+                        FindBy annotation = fieldType.getAnnotation(FindBy.class);
+                        if (annotation != null) {
+                            BaseLocatedElement element = (BaseLocatedElement) fieldType
+                                    .getDeclaredConstructor(Page.class, String.class)
+                                    .newInstance(page, annotation.selector());
+                            field.set(this, element);
+                        }
                     }
                 }
             } catch (Exception e) {
-                e.getMessage();
                 throw new RuntimeException("Не удалось инициализировать поле: " + field.getName(), e);
             }
         }
